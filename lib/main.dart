04835acb7c78f20ecd05597f90eb4ee84ac13f78
GUYwithPTSD <ivandarma3000@gmail.com>
2026-02-27 10:11:03 +0700

    tugas : Buat folder yang rapih dengan misahkan model dan view

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:stateman/home_page.dart';
-import 'package:stateman/counter_model.dart';
+import 'package:stateman/view/home_page.dart';
+import 'package:stateman/model/counter_model.dart';
 
 void main() {
   runApp(
